ab014bc263e2f690265b2815dc7471da7afb37cf
Wayne <61163734+1983Wayne@users.noreply.github.com>
2021-11-11 19:36:50 -0700

    Update Prog1.py

--- Prog1.py
+++ Prog1.py
@@ -1,6 +1,6 @@
 # Math Functions project for Athabasca University COMP 200
 # 11 November 2021
-# Version 1.0
+# Version 1.1
 # Wayne Caissie
 
 border_amount = 30
@@ -18,41 +18,42 @@ def main():
     print("""Welcome, this program will take two integers of your choosing and
 will conduct a series of mathematical operations on them.\n""")
 
-    choice()                                                            #Start the flow of control
+    choice()                                                                            #Start the flow of control
+        
         
 def begin():
-    a, b = "", ""                                                       #Placeholders only until user inputs something for them
+    a, b = "", ""                                                                       #Placeholders only until user inputs something for them
     print("\n")
     
     while type(a) != int:
-            a = input("What is your first number? >>>  ")               #Collects first and second numbers for the math
+            a = input("What is your first number? >>>  ")                               #Collects first and second numbers for the math
             b = input("What is your second number? >>>  ")
                  
             try:
-                int(a)                                                  #Checks to see if the user keyed legal characters
+                int(a)                                                                  #Checks to see if the user keyed legal characters
                 int(b)
-                a, b = int(a), int(b)                                   #If so, change the variables into integers from string
+                a, b = int(a), int(b)                                                   #If so, change the variables into integers from string
                 
             except:
-                a = ""                                                  #If not, reset both a and b to the placeholder string
+                a = ""                                                                  #If not, reset both a and b to the placeholder string
                 b = ""
-                print("You must select an integer for both!")           #And print out a warning
+                print("You must select an integer for both!")                           #And print out a warning
 
-    print("\nAddition: {} + {} =".format(a, b), str(a + b))             #Conduct the math
+    print("\nAddition: {} + {} =".format(a, b), str(a + b))                             #Conduct the math
     print("Subtraction: {} - {} =".format(a, b), str(a - b))
     print("Multiplication: {} * {} =".format(a, b), str(a * b))
     
     try:
-        print("Division: {} / {} =".format(a, b), str(a / b))           #Check for division by 0 and allow it to happen if not
+        print("Division: {} / {} =".format(a, b), str(a / b))                           #Check for division by 0 and allow it to happen if not
     except:
-        print("You cannot divide by zero!")                             #Prints the division by zero warning
+        print("You cannot divide by zero!")                                             #Prints the division by zero warning
         
     print("Exponentiation: {} ** {} =".format(a, b), str(a ** b))
     
     try:
-        print("Modulo: {} % {} =".format(a, b), str(a % b))             #Checks for modulo by 0 and allow it to happen if not
+        print("Modulo: {} % {} =".format(a, b), str(a % b))                             #Checks for modulo by 0 and allow it to happen if not
     except:
-        print("You cannot modulo by zero either!!!")                    #Prints another warning if it is by 0
+        print("You cannot modulo by zero either!!!")                                    #Prints another warning if it is by 0
         
     print("\n")
 
@@ -61,8 +62,9 @@ def begin():
 def goodbye():
     print("Goodbye! See you next time.")
 
+    
 def choice():
-    user_choice = "unknown"                                             #Checks for willingness to participate
+    user_choice = "unknown"                                                             #Checks for willingness to participate
     while user_choice == "unknown":
         user_choice = input("Do you wish to continue? (y/n) >>> ").lower()
         if user_choice == "y" or user_choice == "yes":
@@ -74,7 +76,7 @@ def choice():
             print("You must select y for yes or n for no.")
     
 
-def student_info():                                                     #Project information for professor
+def student_info():                                                                     #Project information for professor
     """This function will print out the particulars for the project start."""
     print("\n")
     print("#" * border_amount)
